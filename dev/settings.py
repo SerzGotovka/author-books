e9348a3016c039fe1716234119f3a78ca7fc2730
Сергей Готовка <112329009+SerzGotovka@users.noreply.github.com>
2024-08-10 14:42:41 +0300

    fix database

--- dev/settings.py
+++ dev/settings.py
@@ -98,7 +98,9 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
+
+if os.getenv('RENDER'):    
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 
 
 
